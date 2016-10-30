3971c1282bc5c2ba61d56d1d282f9c9b50df4e33
Deepanker Aggarwal <deepanker.aggarwal@gmail.com>
2016-10-30 09:32:44 +0000

    Complete Part-A programs with custom Hash Partitioning

---
+++ Part-A/sbt-proj-question-2/build-conf.sbt
@@ -0,0 +1,10 @@
+name := "Part-A-Question-2"
+
+version := "1.0"
+
+scalaVersion := "2.10.6"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.0.0",
+  "org.apache.spark" %% "spark-sql" % "2.0.0"
+)
--- Part-A/sbt-proj-question-3/src/main/scala/Part-A-Question-3.scala
+++ Part-A/sbt-proj-question-3/src/main/scala/Part-A-Question-3.scala
@@ -1,7 +1,8 @@
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.HashPartitioner
 
-object PartAQuestion1 {
+object PartAQuestion3 {
 
   def main(args: Array[String]) {
     if (args.length < 1) {
@@ -11,7 +12,7 @@ object PartAQuestion1 {
 
     val sparkSession = SparkSession
       .builder
-      .appName("CS-838-Assignment2-PartA-1")
+      .appName("CS-838-Assignment2-PartA-3")
       .config("spark.eventLog.enabled", "true")
       .config("spark.eventLog.dir", "hdfs:///tmp/spark-events")
       .config("spark.driver.memory", "1g")
@@ -23,13 +24,15 @@ object PartAQuestion1 {
     val num_iterations = if (args.length > 1) args(1).toInt else 10  // Default number of iterations is 10.
     val num_partitions = 40  // Number of partitions is set to 40
 
+    val customPartitioner = new HashPartitioner(num_partitions)
+
     // Load the dataset into a partitioned RDD assuming that the input data is uncompressed.
     val lines = sparkSession.sparkContext.textFile(args(0), num_partitions)  
 										     
     val links = lines.map{ line =>
       val urls = line.split("\\s+")
       (urls(0), urls(1))
-    }.groupByKey()
+    }.groupByKey(customPartitioner).cache()
 
     var ranks = links.mapValues(rank => 1.0)  // Initial rank is set to 1.0.
 
@@ -37,8 +40,9 @@ object PartAQuestion1 {
       val contribs = links.join(ranks).values.flatMap{ case (urls, rank) =>
         val size = urls.size
         urls.map(url => (url, rank / size))
-      }
-      ranks = contribs.reduceByKey((a,b) => a + b).mapValues(sum_contribs => 0.15 + 0.85 * sum_contribs)  // Update rank.
+      }.partitionBy(customPartitioner)
+
+      ranks = contribs.reduceByKey(customPartitioner, (a,b) => a + b).mapValues(sum => 0.15 + 0.85 * sum)  // Update rank.
     }
 
     val results = ranks.collect()
