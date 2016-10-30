00e5507d85c42a76e7254c9c65d257e4979bf19e
Deepanker Aggarwal <deepanker.aggarwal@gmail.com>
2016-10-30 09:07:48 +0000

    Implemented Part b question 2

---
+++ Part-B/proj-question-2/build-conf.sbt
@@ -0,0 +1,10 @@
+name := "Part-B-Question-2"
+
+version := "1.0"
+
+scalaVersion := "2.10.6"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.0.0",
+  "org.apache.spark" %% "spark-sql" % "2.0.0"
+)
---
+++ Part-B/proj-question-2/src/main/scala/Part-B-Question-2.scala
@@ -0,0 +1,41 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.streaming.ProcessingTime
+import org.apache.spark.sql.DataFrameWriter
+
+object PartBQuestion2 {
+	def main(args: Array[String]) {
+	val inputDirectory = args(0)
+	val outputDirectory = args(1)
+	val spark = SparkSession
+	    .builder
+	    .appName("CS-838-Assignment2-PartB-2")
+	    .config("spark.eventLog.enabled","true")
+	    .config("spark.eventLog.dir","hdfs:///tmp/spark-events")
+	    .config("spark.driver.memory","1g")
+	    .config("spark.executor.cores","4")
+	    .config("spark.task.cpus","1")
+	    .config("spark.sql.streaming.checkpointLocation", "/user/ubuntu/part-b/checkpoint")
+	    .getOrCreate()
+	import spark.implicits._
+	val tweetSchema = new StructType()
+	    .add("userA", "string")
+	    .add("userB", "string")
+	    .add("timestamp", "string")
+	    .add("interaction", "string")
+	val df = spark.readStream
+	    .option("sep", ",")
+	    .schema(tweetSchema)
+	    .csv(inputDirectory)
+	val mtUsers = df.select("userB")
+	    .where("interaction = 'MT'")
+	val query = mtUsers.writeStream
+	    .format("parquet")	    
+	    .trigger(ProcessingTime.create("10 seconds"))
+	    .option("truncate", "false")
+	    .option("numRows", 2147483646)
+	    .start(outputDirectory)
+	query.awaitTermination()
+	}
+}
