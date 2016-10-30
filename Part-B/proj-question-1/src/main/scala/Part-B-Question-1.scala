b14c7b9025afd04aa5d29d195576dc0821592009
Deepanker Aggarwal <deepanker.aggarwal@gmail.com>
2016-10-30 02:30:24 +0000

    Added code for Part-B-Question-1

---
+++ Part-B/proj-question-1/src/main/scala/Part-B-Question-1.scala
@@ -0,0 +1,41 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+object PartBQuestion1 {
+	def main(args: Array[String]) {
+	if (args.length < 3) {
+	}
+	val spark = SparkSession
+	    .builder
+	    .appName("CS-838-Assignment2-PartB-1")
+	    .config("spark.eventLog.enabled","true")
+	    .config("spark.eventLog.dir","hdfs:///tmp/spark-events")
+	    .config("spark.driver.memory","1g")
+	    .config("spark.executor.cores","4")
+	    .config("spark.task.cpus","1")
+	    .getOrCreate()
+	import spark.implicits._
+	val tweetSchema = new StructType()
+	    .add("userA", "string")
+	    .add("userB", "string")
+	    .add("timestamp", "string")
+	    .add("interaction", "string")
+	val df = spark.readStream
+	    .option("sep", ",")
+	    .schema(tweetSchema)
+	    .csv("hdfs:///user/workload/part-b/split-dataset")
+	val windowedCounts = df.groupBy(
+	    		   window($"timestamp", "60 minutes", "30 minutes")
+	    		   , $"interaction")
+		.count()
+		.orderBy("window")
+	val query = windowedCounts.writeStream
+	    .queryName("tweets")
+	    .outputMode("complete")
+	    .format("console")
+	    .option("truncate", "false")
+	    .start()
+	query.awaitTermination()
+	}
+}
