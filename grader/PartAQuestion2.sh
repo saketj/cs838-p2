19b714247ab238e249f2a25eec5390591e754a33
Deepanker Aggarwal <deepanker.aggarwal@gmail.com>
2016-11-07 18:12:54 +0000

    Added lineage pngs

--- grader/PartAQuestion2.sh
+++ grader/PartAQuestion2.sh
@@ -7,4 +7,6 @@ numberOfIterations=10
 numberOfPartitions=40
 jarFile=/home/ubuntu/cs838-p2/Part-A/sbt-proj-question-2/target/scala-2.10/part-a-question-2_2.10-1.0.jar
 echo Running Spark Code
-spark-submit --class "PartAQuestion2" --master $sparkMasterURL $jarFile $dataset $numberOfPartitions $numberOfIterations
+spark-submit --class "PartAQuestion2" --master $sparkMasterURL $jarFile $dataset $numberOfPartitions $numberOfIterations > PartAQuestion2Output.txt 2>&1
+cat PartAQuestion2Output.txt
+
